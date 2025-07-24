d40c937871e159e21ebc0ffd769e61ccf61d006b
Coumbaseck <coumba.seck@univ-thies.sn>
2025-07-24 22:24:19 +0000

    commit

--- frontend-xarala/eslint.config.mjs
+++ frontend-xarala/eslint.config.mjs
@@ -2,13 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-
-export default  {
-  role:{
+export default {
   extends: ["next", "next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
   },
-}
-
-}
+};
